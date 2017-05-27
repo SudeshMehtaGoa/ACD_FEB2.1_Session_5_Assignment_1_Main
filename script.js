7ff5612bc11cdeaf3446a63e7ac29278919c7971
Mehta Sudesh <Mehta@iazi.ch>
2017-05-27 08:07:56 +0200

    Session 5 Assignment 1

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+    Storing values in object and printing them in console.
+*/
+
+function firstFunction() {
+    var PersonData = {
+        name : "Herry Potter",
+        age : 30,
+        dateOfBirth : "10.06.1987",
+        placeOfBirth : "London"
+    };
+    console.log("Name :" + PersonData.name);
+    console.log("Age :" + PersonData.age);
+    console.log("Date of Birth :" + PersonData.dateOfBirth);
+    console.log("Place of Birth :"  + PersonData.placeOfBirth);
+}
